107dc2303e3b1141ad125303aaddd4d65c6bdf2b
Ale Breso <psbreso@gmail.com>
2019-02-20 15:23:40 +0100

    getting closer

--- src/components/Row.vue
+++ src/components/Row.vue
@@ -4,11 +4,11 @@
     <span class="left">row {{row.row}}</span>
     <app-seat
       v-for="({seat},i) in row.seats"
-      :seat="seat"
+      :currentSeat="seat"
       :groups="groups"
       :row="row"
       :key="i"
-      :class="isSeatTaken(seat)"
+      :class="{orange:attachOrange, red:attachRed}"
     ></app-seat>
     <hr>
   </div>
@@ -19,7 +19,8 @@ export default {
   props: ["row", "groups"],
   data: function() {
     return {
-      color: ""
+      attachOrange: false,
+      attachRed: false
     };
   },
   components: {
@@ -27,28 +28,36 @@ export default {
   },
   methods: {
     isSeatTaken(currentSeat) {
-      this.groups.forEach(group => {
-        group.seats.forEach(seat => {
-          console.log("row.row === seat.row");
-          console.log(`${parseInt(this.row.row)} === ${parseInt(seat.row)}`);
-          if (parseInt(this.row.row) === parseInt(seat.row)) {
-            console.log('inside I if')
-            console.log("seat.seat === currentSeat");
-            console.log(`${seat.seat} === ${currentSeat}`);
-            if (parseInt(seat.seat) === parseInt(currentSeat)) {
-              console.log('inside II if')
-              console.log("seat.seat === currentSeat");
-              console.log(`${group.id} === +31611111111`);
-              if (group.id === "+31611111111") {
-                console.log('inside III if')
-                return { orange: true };
-              }
-              else return { red: true };
-            }
-          }
-        });
-      });
-      return { orange: false, red:false};
+      // this.groups.forEach(group => {
+      //   group.seats.forEach(seat => {
+      //     console.log("row.row === seat.row");
+      //     console.log(`${parseInt(this.row.row)} === ${parseInt(seat.row)}`);
+      //     if (parseInt(this.row.row) === parseInt(seat.row)) {
+      //       console.log('inside I if')
+      //       console.log("seat.seat === currentSeat");
+      //       console.log(`${seat.seat} === ${currentSeat}`);
+      //       if (parseInt(seat.seat) === parseInt(currentSeat)) {
+      //         console.log('inside II if')
+      //         console.log("seat.seat === currentSeat");
+      //         console.log(`${group.id} === +31611111111`);
+      //         if (group.id === "+31611111111") {
+      //           console.log('inside III if')
+      //           this.attachOrange = true
+      //           this.attachRed = false
+      //           return 
+      //         }
+      //         else {
+      //           this.attachOrange = false
+      //           this.attachRed = true
+      //           return 
+      //         }
+      //       }
+      //     }
+      //   });
+      // });
+      // this.attachRed=false
+      // this.attachOrange=false
+      // return
     }
   }
 };
--- src/components/Seat.vue
+++ src/components/Seat.vue
@@ -1,15 +1,50 @@
 <template>
-  <div class="text-center seat">{{seat}}</div>
+  <div class="text-center seat" :class="{red:attachRed,orange:attachOrange}">{{currentSeat}}</div>
 </template>
 <script>
 export default {
   data: function() {
     return {
+      attachOrange: true,
+      attachRed: false
     };
   },
-  props: ["seat", "row", "groups"],
+  props: ["currentSeat", "row", "groups"],
   methods: {
   },
+  created() {
+    const currentSeat = this.currentSeat
+    this.groups.forEach(group => {
+        group.seats.forEach(seat => {
+          console.log("row.row === seat.row");
+          console.log(`${parseInt(this.row.row)} === ${parseInt(seat.row)}`);
+          if (parseInt(this.row.row) === parseInt(seat.row)) {
+            console.log('inside I if')
+            console.log("seat.seat === currentSeat");
+            console.log(`${seat.seat} === ${currentSeat}`);
+            if (parseInt(seat.seat) === parseInt(currentSeat)) {
+              console.log('inside II if')
+              console.log("seat.seat === currentSeat");
+              console.log(`${group.id} === +31611111111`);
+              if (group.id === "+31611111111") {
+                console.log('inside III if')
+                this.attachOrange = true
+                this.attachRed = false
+                return 
+              }
+              else {
+                this.attachOrange = false
+                this.attachRed = true
+                return 
+              }
+            }
+          }
+        });
+      });
+      // this.attachRed=false
+      // this.attachOrange=false
+      return
+  }
 };
 </script>
 <style scoped>
@@ -19,5 +54,4 @@ div {
   width: 50px;
   height: 50px;
 }
-
 </style>
